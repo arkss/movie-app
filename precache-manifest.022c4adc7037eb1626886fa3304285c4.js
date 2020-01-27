b12b66774ca2cdf980d2af681a778d2c47c50108
minsung <rkdalstjd9@naver.com>
2020-01-27 16:57:27 +0900

    Updates

---
+++ precache-manifest.022c4adc7037eb1626886fa3304285c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51482ca225af2c57e9d0ae4784e7ca78",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "6087165192b8eb0de65e",
+    "url": "/movie-app/static/js/2.0b73dcc9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie-app/static/js/2.0b73dcc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e054b6665301d22294e",
+    "url": "/movie-app/static/js/main.2ee28d40.chunk.js"
+  },
+  {
+    "revision": "60f14217498504fcfb6a",
+    "url": "/movie-app/static/js/runtime-main.8e14a1ed.js"
+  }
+]);
